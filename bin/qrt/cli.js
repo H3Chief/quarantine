f44a6d26c541669539dd180c2753fdfe3bcd598d
Brian Vanderbusch <brian@normalgravity.com>
2015-08-02 18:44:36 -0500

    adding some process info

--- bin/qrt/cli.js
+++ bin/qrt/cli.js
@@ -1,1 +1,9 @@
-#!/usr/local/node
+#!/usr/bin/env node
+process.title = "qrt";
+var parseArgs = require("minimist");
+
+var argv = parseArgs(process.argv.slice(1),{
+	'--':true
+});
+
+console.log("command to execute", argv);
